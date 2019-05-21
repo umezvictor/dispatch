1c1be1faa8eb54cd04f70f38cfdf3b2a094463eb
Victor Umezuruike <41339694+umezvictor@users.noreply.github.com>
2019-05-21 12:29:19 +0100

    Update keys.js
    Removed sensitive info

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,11 @@
 //mobgodb connection
 module.exports = {
-    //MongoURI: 'mongodb+srv://victorblaze:D1PW36btWDhhAFsP@cluster0-ygkqe.mongodb.net/test?retryWrites=true',
+    //MongoURI: 'mongodb+srv://username:D1PW36btWDhhAFsP@cluster0-ygkqe.mongodb.net/test?retryWrites=true',
     
-    //MongoURI: 'mongodb://victorblaze:#1989Vic.@ds123645.mlab.com:23645/companiesdb',
-    MongoURI: 'mongodb://localhost:27017/dispatchDB',
+    //MongoURI: 'mongodb://myusername:mypassword@ds123645.mlab.com:23645/companiesdb',
+    MongoURI: 'mongodb://localhost:27017/dispatchDB',//my local connection using atlas
     URL: process.env.BASE_URL || 'http://localhost:3050',
     ENV: process.env.NODE_ENV || 'development',
-    JWT_SECRET: process.env.JWT_SECRET || 'mypassword',
-    SendGridKey1: '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'
+    JWT_SECRET: process.env.JWT_SECRET || '****',
+    SendGridKey1: '*****'
 };
